3cc8e681f4d53a677af6def41f14fd8593349351
metinis <denis.muraska@gmail.com>
2025-05-24 18:41:13 +0100

    Seperate into submodules

--- ZeusEditor/src/imgui/ImGUILayerVulkan.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ImGUILayer.h"
-#include "GLFW/glfw3.h"
-
-class ImGUILayerVulkan : public ImGUILayer{
-    void Init(GLFWwindow* window) override;
-    void Shutdown() override;
-    void Render() override;
-};
